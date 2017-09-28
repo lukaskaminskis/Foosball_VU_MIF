32f0dbd7ab2058bbd98908cfec4fdf61fb442fc2
Deimantas Gedgaudas <pawlikas77@gmail.com>
2017-09-28 11:09:40 +0300

    Add basic user class

---
+++ Foosball_MIF/Foosball_Lib/User/User.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Foosball_Lib.User
+{
+    public class User
+    {
+        private string UserId { get; set; }
+        private string Password { get; set; }
+        private int MatchCount { get; set; }
+        private int Wins { get; set; }
+        private int Loses { get; set; }
+
+        public User(string _userId, string _password)
+        {
+            this.UserId = _userId;
+            this.Password = _password;
+        }
+
+        public void ChangePassword(string _Password)
+        {
+            this.Password = _Password;
+        }
+
+        public void MatchWon(string _UserId)
+        {
+            MatchCount++;
+            Wins++;
+        }
+
+        public void MatchLost(string _UserId)
+        {
+            MatchCount++;
+            Loses++;
+        }
+}
+}
