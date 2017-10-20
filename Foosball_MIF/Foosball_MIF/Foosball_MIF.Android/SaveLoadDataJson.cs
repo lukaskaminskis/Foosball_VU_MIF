13d3a10c4d1d0857a255b53ef1098b5dde823588
Kiziela Tomas <tomas.kiziela@mif.stud.vu.lt>
2017-10-20 23:26:44 +0300

    Added a class for saving an object in json format to .txt and back. Issues: You can only add objects, not delete them and reading won't work with multiple objects.

---
+++ Foosball_MIF/Foosball_MIF/Foosball_MIF.Android/SaveLoadDataJson.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+using System;
+using Xamarin.Forms;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Foosball_Lib.Models;
+
+namespace Foosball_MIF.Droid
+{
+    class SaveLoadDataJSON
+    {
+
+        public SaveLoadDataJSON()
+        {
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+            string filePath = Path.Combine(path, "UserData.txt");
+
+            void SaveJSON(Object Data)
+            {
+                string json = JsonConvert.SerializeObject(Data);
+                using (var file = File.Open(filePath, FileMode.Append, FileAccess.Write))
+                using (var strm = new StreamWriter(file))
+                {
+                    strm.Write(json);
+                }
+            }
+
+            Object LoadJSON()
+            {
+                string json;
+                using (var file = File.Open(filePath, FileMode.Open))
+                using (var strm = new StreamReader(file))
+                {
+                    json = strm.ReadToEnd();
+                }
+                return JsonConvert.DeserializeObject(json);
+            }
+        }
+    }
+}
