f0bb86f513cdba5ea474115cc54ba4552149cdca
Kaminskis Lukas <lukas.kaminskis@mif.stud.vu.lt>
2017-11-20 15:56:30 +0200

    * Foosball_Lib.csproj: Added video and photo uploading and reading
    * packages.config:
    * PropertiesPage.xaml:
    * PropertiesPage.xaml.cs:
    * UploadVisualContent.xaml:
    * UploadVisualContent.xaml.cs:
    * MainActivity.cs:
    * packages.config:
    * MainApplication.cs:
    * file_paths.xml:
    * Foosball_MIF.Android.csproj:
    * file_paths.xml:
    * AndroidManifest.xml:
    * Resource.Designer.cs:

--- Foosball_MIF/Foosball_MIF/Foosball_MIF.Android/MainActivity.cs
+++ Foosball_MIF/Foosball_MIF/Foosball_MIF.Android/MainActivity.cs
@@ -6,22 +6,33 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Plugin.Media;
+using Plugin.Permissions;
+using Plugin.Permissions.Abstractions;
 
 namespace Foosball_MIF.Droid
 {
 	[Activity (Label = "Foosball_MIF", Icon = "@drawable/icon", Theme="@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
 	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
-	{
-		protected override void OnCreate (Bundle bundle)
+    {
+        
+		protected override  void OnCreate (Bundle bundle)
 		{
 			TabLayoutResource = Resource.Layout.Tabbar;
 			ToolbarResource = Resource.Layout.Toolbar; 
 
 			base.OnCreate (bundle);
 
+
+            CrossMedia.Current.Initialize();
+
+
 			global::Xamarin.Forms.Forms.Init (this, bundle);
 			LoadApplication (new Foosball_MIF.App ());
 		}
+
+       
 	}
+
 }
 
